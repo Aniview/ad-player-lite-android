c639c3ef91019c950c743915837aed35bbcebc08
matrixdev <www.matrix.dev@gmail.com>
2025-12-15 11:10:35 +0200

    version 2.9.0

--- app/src/main/java/com/adservrs/adplayer/lite/example/MainActivity.kt
+++ app/src/main/java/com/adservrs/adplayer/lite/example/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.adservrs.adplayer.lite.example.ui.theme.AdplayerliteandroidexampleTheme
 
-const val PUB_ID = "565c56d3181f46bd608b459a"
-const val TAG_ID = "67dd7d7835b4a37b880f2b3c"
+//const val PUB_ID = "565c56d3181f46bd608b459a"
+//const val TAG_ID = "67dd7d7835b4a37b880f2b3c"
+const val PUB_ID = "61235240ea13f3415e2ab496"
+const val TAG_ID = "673220403d68bf7004069617"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
