ea969d0be8899861254bf3b551f576d05a374ce5
matrixdev <www.matrix.dev@gmail.com>
2026-06-16 14:55:20 +0300

    version 3.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
     //noinspection UseTomlInstead
-    implementation("com.adservrs:ad-player-lite:3.8.0")
+    implementation("com.adservrs:ad-player-lite:3.9.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
